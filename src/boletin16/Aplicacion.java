8046e711708f93be603ea2d833da2b09dc6e7ad7
ssahuquilloembade <ssahuquilloembade@victoria10.danielcastelao.org>
2018-01-10 08:47:37 +0100

    Se añade el llamar al metodo calcular nota

--- src/boletin16/Aplicacion.java
+++ src/boletin16/Aplicacion.java
@@ -7,6 +7,7 @@ public class Aplicacion {
        Persoal persoal1 = new Persoal("Sergio","986986986");
        Academia alumno1 = new Academia(2018, "Sergio",8, persoal1);
         System.out.println(alumno1.toString());
+        System.out.println(alumno1.calcularNota());
     
     
     
